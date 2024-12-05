0c56397fb7299c5144823e6ec07a7c8b195c4849
Shayna Chambless <shaychamblessshay@gmail.com>
2024-12-05 10:41:38 -0800

    refactor

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ import time
 import cotask
 import task_share
 from pyb import Pin, Timer
-from time import ticks_us, ticks_add, ticks_diff, sleep
+from time import ticks_us, ticks_diff
 import math
 from bno055 import *
-from pyb import I2C, ADC
 import machine
+from classes import BNO055_2, Encoder, Motor
+import task_share
+import cotask
 
 """
 CLASSES
 """
-
-import task_share
-import cotask
-
 
 class TaskManager:
     def __init__(self, motorL, motorR, encL, encR, IMU):
@@ -476,141 +474,9 @@ class TaskManager:
 
         return (vleft*2.4), (vright*2.4)
 
-
-# Encoder class to read and update the encoder ticks
-class Encoder:
-    # Initialize Encoders
-    def __init__(self, tim, pin_A, pin_B, ch1, ch2):
-        self.zero()
-        self.tim = tim
-        self.pin_A = pin_A
-        self.pin_B = pin_B
-        # Set timer channels and pins accordingly
-        self.tim.channel(ch1, pin=pin_A, mode=Timer.ENC_AB)
-        self.tim.channel(ch2, pin=pin_B, mode=Timer.ENC_AB)
-        self.current_time = time.ticks_us()
-
-        self.update()
-
-    # Update Encoder Values
-    def update(self):
-        self.old_val = self.current_val  # Reset old value
-        self.old_time = self.current_time
-        self.current_time = time.ticks_us()
-        self.current_val = self.tim.counter()  # Grab current ticks value
-        self.delta = self.current_val - self.old_val  # get delta to caluclate speed
-        self.deltat = ticks_diff(self.current_time, self.old_time)
-
-        # Handle overflow or underflow
-        if self.delta < -32768:
-            self.delta += 65536
-        elif self.delta > 32768:
-            self.delta -= 65536
-        # Keep track of linear position of each encoder, basically how far each encoder has gone
-        self.position += self.delta
-
-    # Geting the updated positon
-    def get_position(self):
-        return self.position
-
-    # Get updated delta
-    def get_delta(self):
-        return self.delta
-
-    # Define Zero Location for Linear Posiition
-    def zero(self):
-        self.current_val = 0
-        self.old_val = 0
-        self.delta = 0
-        self.position = 0
-
-    def get_velocity(self):
-        self.velocity = (self.delta * math.pi) / (self.deltat * 720 / 1000000)  # retunrs rad/s
-        return self.velocity
-
-    # Gets position of each motor and prints it when able
-    def task_get_position(self, shares):
-        the_share, the_queue = shares
-        while True:
-            self.update()
-            if not the_queue.full():
-                the_queue.put(self.get_position())
-            yield 0
-
-
-# Motor driver class
-class Motor:
-    # Intiilization
-    def __init__(self, tim, phase, enable, sleep):
-        self.PH = phase
-        self.tim = tim
-        self.sleep = sleep
-        self.EN = self.tim.channel(1, mode=Timer.PWM, pin=enable)
-        self.EN.pulse_width_percent(0)
-        self.sleep.high()
-        self.duty = 0
-
-    # Sets speed of each motor/wheel
-    def set_duty(self, pwm):
-        self.duty = pwm
-        if pwm > 0:
-            self.PH.low()
-            self.EN.pulse_width_percent(pwm)
-        elif pwm < 0:
-            self.PH.high()
-            self.EN.pulse_width_percent(abs(pwm))
-        else:
-            self.EN.pulse_width_percent(0)
-
-    # Enable the motor
-    def enable(self):
-        self.sleep.high()
-
-    # Disable the Motor
-    def disable(self):
-        self.sleep.low()
-
-    def task_run_motor(self, shares):
-        while True:
-            the_share, the_queue = shares
-            pos = the_queue.get()
-            the_queue.put(pos)
-            if (pos < 30000):
-                if self.duty == 0:
-                    self.set_duty(50)
-                yield
-            else:
-                self.set_duty(0)
-                yield
-
-
-class BNO055_2(BNO055):
-    def __init__(self, i2c, address=0x28, crystal=True, transpose=(0, 1, 2), sign=(0, 0, 0)):
-        super().__init__(i2c, address, crystal, transpose, sign)
-        self.curr = time.time_ns()
-        self.angle_prev = self.euler()[0]
-        self.prev = self.curr
-
-    def get_heading_velocity(self):
-        self.prev = self.curr
-        self.curr = time.time_ns()
-
-        time_interval = time.ticks_diff(self.curr, self.prev)
-        angle_curr = self.euler()[0]
-        angle_diff = 2 * math.pi * (angle_curr - self.angle_prev)
-        self.angle_prev = angle_curr
-
-        angle_velocity = angle_diff / time_interval
-        return angle_velocity
-
-    def get_yaw_velocity(self):
-        return -1 * self.get_heading_velocity()
-
-
 """
 FUNCTIONS
 """
-
 
 # Set up the motors in accordance with the Motor class
 def initialize_motors():
@@ -637,57 +503,6 @@ def initialize_encoders():
     enc_L = Encoder(tim_L, Pin.cpu.A7, Pin.cpu.A6, 1, 2)
     return enc_R, enc_L
 
-
-# Creates Tasks and appends them to schedualing list
-def create_tasks(enc_R, enc_L):
-    share0 = task_share.Share('b', thread_protect=True, name="Share 0")
-    q = task_share.Queue('l', 16, thread_protect=True, overwrite=False, name="Queue 0")
-    # Input 2 Zeros for R/L motor initial position
-    # task for printing to console
-    print_positions_task = cotask.Task(task_print_motor_data, name='Print Motor Data', priority=0, period=100,
-                                       profile=True, trace=False, shares=(share0, q))
-    # task for reading right motor position adn add to queue
-    readR_task = cotask.Task(enc_R.task_get_position, name='Read Motor Right', priority=2, period=100,
-                             profile=True, trace=False, shares=(share0, q))
-    # task for reading left motor position and add to queue
-    readL_task = cotask.Task(enc_L.task_get_position, name='Read Motor Left', priority=2, period=100,
-                             profile=True, trace=False, shares=(share0, q))
-    # tasks for running each motor
-    Run_Motor_R = cotask.Task(motor_R.task_run_motor, name="Run Motor R", priority=1, period=100, profile=True,
-                              trace=False, shares=(share0, q))
-    Run_Motor_L = cotask.Task(motor_L.task_run_motor, name="Run Motor L", priority=1, period=100,
-                              profile=True, trace=False, shares=(share0, q))
-    # Add tasks to task_list
-    cotask.task_list.append(print_positions_task)
-    cotask.task_list.append(Run_Motor_R)
-    cotask.task_list.append(Run_Motor_L)
-    cotask.task_list.append(readR_task)
-    cotask.task_list.append(readL_task)
-
-
-# Function that runs the tasks? Can't we just put this in main?
-def run_tasks():
-    while True:
-        try:
-            cotask.task_list.pri_sched()
-        except KeyboardInterrupt:
-            break
-
-
-"""
-TASKS
-"""
-
-
-# This tasks prints the motor data continuously
-def task_print_motor_data(shares):
-    the_share, the_queue = shares
-    while True:
-        if the_queue.empty():
-            print("empty queue")
-        else:
-            print(the_queue.get(), the_queue.get())
-        yield 0
 
 
 if __name__ == '__main__':
